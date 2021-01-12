b8a814f40313bd2efc2a5f9faec44d874268c8f1
jekr <48930765+a7mdal3le@users.noreply.github.com>
2021-01-12 05:57:15 +0300

    Create m2.js

---
+++ m2.js
@@ -0,0 +1,180 @@
+var _0x7fae=[".nav-tabs","insertBefore","<form name="jump">
+<div class="MyColors">
+					<map id="ColorsMap" name="ColorsMap">
+					<area alt="" coords="275, 0, 298, 25" href="javascript:chooseStyle('none',60)" shape="rect" title="1">
+					<area alt="" coords="244, 0, 268, 25" href="javascript:chooseStyle('Co2',60)" shape="rect" title="2">
+					<area alt="" coords="213, 0, 238, 25" href="javascript:chooseStyle('Co3',60)" shape="rect" title="3">
+					<area alt="" coords="183, 0, 207, 25" href="javascript:chooseStyle('Co4',60)" shape="rect" title="4">
+					<area alt="" coords="152, 0, 177, 25" href="javascript:chooseStyle('Co5',60)" shape="rect" title="5">
+					</map>
+			        <img alt="" height="40" src="https://1.top4top.net/p_1388gqx1c1.gif" width="300" usemap="#ColorsMap"></div>
+</form>
+
+
+
+
+
+","https://rawcdn.githack.com/waleed-qaid/des/be9e1d25a3f5960e1ab57e2dca7fd843952cdfbe/www.1des.net-chatt-www.s6acl.com-color1.js","getScript",".loginstat","insertAfter","<div class='fr borderg' style='background-color:white;float:left;'> <a href='https://www.al3nood.net/'>شات العنود</a></div>","<input id="stealth" type="checkbox" value="" maxlength="12"><i class="fa fa-eye-slash" aria-hidden="true">","html",".checkbox label","body","<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" href="">
+<link href="https://raw.githack.com/waleed-qaid/1des/master/www.1des.net-chatt-www.rooo3.com1.css" rel="stylesheet" type="text/css" />
+<link href="https://raw.githack.com/waleed-qaid/1des/master/www.1des.net-chatt-www.rooo3.com2.css" media="screen" rel="alternate stylesheet" title="Co2" type="text/css" />
+<link href="https://rawcdn.githack.com/waleed-qaid/1des/a8bc555d8f59b9c36f6ce9781aa088bb10765599/www.1des.net-chatt-www.rooo3.com3.css" media="screen" rel="alternate stylesheet" title="Co3" type="text/css" />
+<link href="https://raw.githack.com/waleed-qaid/1des/master/www.1des.net-chatt-www.rooo3.com4.css" media="screen" rel="alternate stylesheet" title="Co4" type="text/css" />
+<link href="https://raw.githack.com/waleed-qaid/1des/master/www.1des.net-chatt-www.rooo3.com5.css" media="screen" rel="alternate stylesheet" title="Co5" type="text/css" />
+<style>
+
+
+
+.MyColors {
+    position: absolute;
+    transform: matrix(1, 0, 0, 1, 85, 0);
+	margin-top: 2px;
+    width: 300px;
+    height: 40px;
+    float: left;
+    background: transparent url(https://rooo3.com/sico/1597869368254.png) no-repeat scroll right top;
+    text-align: left;
+}
+
+button.btn.fr.btn-success.fa.fa-refresh{
+display:none
+}
+a.label.label-primary.fl img.fl{
+display:none
+}
+a.label.label-primary.fl {border-radius: 3px!important;margin: 2px -156px;padding: 5px!important;margin-bottom: 2px;text-align: right!important;}
+#des22{
+text-align:center!important;
+font-size:100%!important
+}
+label.fl.label.loginstat{
+background-color:transparent!important;
+position:absolute;
+top:0;
+margin-top:4px;
+left:0
+}
+.fr.borderg,.fr.borderg a{
+font-size:10px!important;
+padding:0 5px!important;
+position:relative;
+color:#ffffff
+}
+.fr.borderg{
+border:0;
+margin-top:0;
+height:0
+}
+span.s1.fa.fa-user.label.badgex.label-as-badge.label-success{
+padding:0 3px 2px;
+margin-right:5px
+}
+label.label.label-primary.mini.fl{
+padding:1px;
+border-radius:3px!important
+}
+div#l1 input#u1,div#l3 button{
+width:99%;
+margin-left:1px
+}
+div#l2 button,div#l3 button{
+border:1px solid #ccc!important;
+border-radius:3px!important;
+height:27px;
+padding:0 5px
+}
+div#l1 input#u1{
+border-radius:3px;
+border:1px solid #e1e0e0;
+margin-top:3px;
+text-align:center
+}
+div#l2 input#pass1,div#l2 input#u2,div#l3 input#pass2,div#l3 input#u3{
+width:49%;
+padding:0!important;
+float:right;
+margin:2px 5px 2px 0
+}
+div#l2 input#u2,div#l3 input#u3{
+margin-left:-6px;
+border-radius:3px;
+border:1px solid #e1e0e0;
+text-align:center
+}
+div#l2 input#pass1,div#l3 input#pass2{
+border-radius:3px;
+border:1px solid #e1e0e0;
+text-align:center
+}
+div#l2,div#l3{
+padding:2px!important
+}
+div#l1{
+margin:0 1px -18px;
+padding:0!important
+}
+div#l1,div#l2,div#l3{
+border-bottom:2px solid;
+margin-bottom:2px;
+border-top:2px solid;
+margin-top:0;
+border-color:#bababa;
+border-radius:3px;
+height:69px
+}
+div#l1 button{
+border:1px solid #ccc!important;
+border-radius:3px!important;
+width:99%;
+height:27px;
+padding:0 5px
+}
+div#l2 button{
+width:70%
+}
+ul.nav.nav-tabs>li>a{
+border:none;
+text-align:center;
+border-radius:3px;
+display:initial;
+width:80%;
+float:left;
+padding:0!important
+}
+ul.nav.nav-tabs>li.active>a{
+border:none;
+border-radius:3px
+}
+ul.nav.nav-tabs>li{
+width:32%;
+margin:0 5px 0 0
+}
+ul.nav.nav-tabs>li>.fa-user-plus:before,ul.nav.nav-tabs>li>.fa-user:before,ul.nav.nav-tabs>li>.fa-user:before{
+float:right;
+margin-right:-26%;
+padding:0 2px 1px;
+border-radius:3px;
+width:23px;
+text-align:center
+}
+.nav>li>a:focus,.nav>li>a:hover{
+color:#9abff9
+}
+ul.nav.nav-tabs{
+margin-top:2px;
+float:right;
+width:100%;
+padding:3px 1px
+}
+
+div#tlogins .fl.mini.u-msg{
+margin-top:3px!important;
+border-radius:3px!important
+}
+a.label.label-primary.fl img.fl,button.btn.fr.btn-success.fa.fa-refresh,div#l2 br,div#l3 br{
+display:none
+}
+
+
+
+</style>"];$(`${_0x7fae[2]}`)[_0x7fae[1]](_0x7fae[0]);$[_0x7fae[4]](_0x7fae[3]);$(_0x7fae[7])[_0x7fae[6]]($(_0x7fae[5]));$(_0x7fae[10])[_0x7fae[9]](`${_0x7fae[8]}`);$(`${_0x7fae[12]}`)[_0x7fae[1]](_0x7fae[11])
