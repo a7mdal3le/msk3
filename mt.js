d19f5f8a09276b5895f9cf830d250f5f25dd3453
jekr <48930765+a7mdal3le@users.noreply.github.com>
2021-02-17 06:16:27 +0300

    Create mt.js

---
+++ mt.js
@@ -0,0 +1,34 @@
+
+$('div#tlogins div#l1 input,div#tlogins div#l2 input,div#tlogins div#l3 input').addClass('form-control primaryborder').css({'text-align':'center','max-width':'40%','float': 'right'})
+$('div#l1 br,div#l2 br,div#l3 br').remove()
+$('input#stealth').css('max-width','15px').removeClass('form-control')
+$('#l2 .checkbox label').html($('#l2 .checkbox label input')[0].outerHTML)
+$('#l2 .checkbox').css({"margin":"7px -5px 0 0","float":"right"})
+$('ul.nav.nav-tabs,.nav-tabs>li>a').css({"":"","padding":"4px 7px"})
+$('ul.nav.nav-tabs').css({"padding":"0px"})
+$('input#u1').css({"max-width":"80%"})
+$(".ubnr").remove();
+$('.u-msg.dots').removeClass('dots');
+$(".fa-gear").text("الضبط").css("width","");
+$('.nav-tabs').addClass('fl').css('width','100%');
+$(".checkbox label").html(`<input id="stealth" type="checkbox" value="">`);
+$('.fr.borderg a').text('دوك')
+$("#d0 .fa-gear").html('&nbsp;').css('width','36px');
+$(`<marquee class="label-primary fl" direction="right" width="99%" style="padding-top:0px;font-family: 'jazeera-light',FontAwesome;height: 23px;color: white;border-radius: 2px;margin: 0 2px 2px!important;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"
+> . آفضل زائر لهذا الاسبوع . <span style="background-color: white;border-radius: 10px;padding: 0px 7px 0;margin-top: 1px;border-top: 1px solid #fff0;color: #000"> قريبا </span> . آفضل سوبر لهذا الاسبوع  .<span style="background-color: white;border-radius: 10px;padding: 0px 7px 0;margin-top: 1px;border-top: 1px solid #fff0;color: #000"> قريبا </span> . آفضل سوبر مسابقات لهذا الاسبوع  .<span style="background-color: white;border-radius: 10px;padding: 0px 7px 0;margin-top: 1px;border-top: 1px solid #fff0;color: #000"> قريبا ..ِ  </span> مع تمنياتنا للجميع بـ أطيب الاوقات  " 
+ </marquee>`).insertBefore('.lonline.light.break');
+ $('.hand.nosel.fl.uzr.border').css({'margin': '1px 0',borderRight: '3px solid #FFFFFF',borderLeft: '3px solid #E0E0E0',borderTop: '1px solid #E0E0E0',borderBottom: '1px solid #FFFFFF',paddingTop: '3px',borderRadius: '5px'});
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="">
+<style>
+.fitimg.fl.u-pic  {box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(112, 112, 113);border-radius: 75px 75px 75px 75px;}
+.fr.co {
+    transform: rotate(0deg);
+    height: 10px;
+    width: 16px!important;
+    margin-top: 4px;
+}
+<div class="lonline light break" style="width:100%;outline: lightgray solid 1px;">
+
+
+</style>`).insertBefore('body');
